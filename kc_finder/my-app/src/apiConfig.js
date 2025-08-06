23f88de7f617fb1e5db1d944346b723ceea02f67
donguk585 <137376258+donguk585@users.noreply.github.com>
2025-08-06 20:12:37 +0900

    Update apiConfig.js

--- kc_finder/my-app/src/apiConfig.js
+++ kc_finder/my-app/src/apiConfig.js
@@ -2,8 +2,8 @@
 const DEVELOPMENT_API_BASE_URL = 'http://localhost:8080';
 
 // 배포 환경 (실제 서버)
-const PRODUCTION_API_BASE_URL = 'https://api.yourdomain.com'; // 실제 배포될 도메인으로 변경하세요
+const PRODUCTION_API_BASE_URL = 'http://3.35.17.116'; // 실제 배포될 도메인으로 변경하세요
 
 
 
-export const API_BASE_URL = DEVELOPMENT_API_BASE_URL; // 현재 개발 모드이므로 로컬 URL 사용
+export const API_BASE_URL = PRODUCTION_API_BASE_URL; // 현재 개발 모드이므로 로컬 URL 사용
