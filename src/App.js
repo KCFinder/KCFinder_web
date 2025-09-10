b77aba193a7802f09e781fc033e781363238eb2e
Sutaenghhh <neocop7@gmail.com>
2025-09-10 10:06:10 +0900

    feat : 프로젝트 초기 설정 ( tailwindcss 포함 )

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-extrabold text-red-500">
+        Tailwind v4 Test
+      </h1>
+    </div>
+  );
+}
+
